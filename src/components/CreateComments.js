5b9b4e5f76c8953eb913cd65cab854c6f8b236e8
MinJiwon <jwjwmin111@gmail.com>
2022-09-27 15:24:33 +0900

    :art: input값 상태관리
    input값 상태관리

--- src/components/CreateComments.js
+++ src/components/CreateComments.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -19,11 +20,38 @@ const BottomContents = styled.div`
 `;
 
 function CreateComments() {
+  // 작성자, 댓글내용 상태
+  const [input, setInput] = useState({
+    writer: "",
+    contents: "",
+  });
+  const { writer, contents } = { ...input };
+
+  // input값 핸들러
+  const handleInputChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    console.log("댓글입력창 렌더");
+  });
+
   return (
     <Wrapper>
-      <input type="text" placeholder="작성자" />
+      <input
+        type="text"
+        placeholder="작성자"
+        onChange={handleInputChange}
+        name="writer"
+        value={writer}
+      />
       <BottomContents>
-        <textarea placeholder="댓글내용" />
+        <textarea
+          placeholder="댓글내용"
+          onChange={handleInputChange}
+          name="contents"
+          value={contents}
+        />
         <button>등록</button>
       </BottomContents>
     </Wrapper>
